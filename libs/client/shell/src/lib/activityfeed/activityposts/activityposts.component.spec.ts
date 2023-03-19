52ada48ae4a70a358eaa72a18479d2cd9187ef32
Rob Gorham <rgorham@twelve5designs.com>
2023-03-19 14:29:20 -0400

    Working test for creation

--- libs/client/shell/src/lib/activityfeed/activityposts/activityposts.component.spec.ts
+++ libs/client/shell/src/lib/activityfeed/activityposts/activityposts.component.spec.ts
@@ -1,20 +1,48 @@
 import { createComponentFactory, Spectator } from '@ngneat/spectator';
 import { PoisComponent } from './activityposts.component';
+import { RouteService } from '@involvemint/client/shared/routes';
 import { IonicModule } from '@ionic/angular';
-import { ImBlockComponent } from 'libs/client/shared/data-access/src/lib/smart-components/im-block/im-block.component';
+import { CommentService, ImBlockModule, UserFacade } from '@involvemint/client/shared/data-access';
+import { ChangeMakerFacade, EnrollmentsModalService } from '@involvemint/client/cm/data-access';
+import { of } from 'rxjs';
 
 fdescribe('Activity Posts Component', () => {
+  let userFacade: UserFacade;
   let spectator: Spectator<PoisComponent>;
+
   const createComponent = createComponentFactory(
     {
-      component: PoisComponent, 
-      imports: [IonicModule.forRoot()],
-      declarations: [ImBlockComponent],
-      });
+      component: PoisComponent,
+      imports: [IonicModule.forRoot(), ImBlockModule],
+      mocks: [
+        RouteService,
+        ChangeMakerFacade,
+        EnrollmentsModalService,
+        UserFacade,
+        CommentService
+      ],
+     
 
+      // declarations: [ImBlockComponent],
+    });
+
+  beforeEach(() => {
+    userFacade = {
+      posts: {
+        selectors: {
+          posts$: of([])
+        }
+      }
+    } as any;
+    spectator = createComponent({
+      providers: [
+        { provide: UserFacade, useValue: userFacade }]
+    });
+
+    });
   it('should create', () => {
-    spectator = createComponent();
 
     expect(spectator.component).toBeTruthy();
+    expect(true).toEqual(false);
   });
 });
