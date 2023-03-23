3fc0da329b6da792eab2a909c7397f8537802dec
Brad Williams <bwillia2@andrew.cmu.edu>
2023-03-23 13:44:15 -0400

    comments show handle of author's name and profile picture

--- libs/client/shell/src/lib/activityfeed/activityposts/comments/modal-comments.component.ts
+++ libs/client/shell/src/lib/activityfeed/activityposts/comments/modal-comments.component.ts
@@ -1,65 +1,73 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { PostStoreModel, UserFacade } from '@involvemint/client/shared/data-access';
-import { StatefulComponent } from '@involvemint/client/shared/util';
-import { Handle, ViewProfileInfoQuery } from '@involvemint/shared/domain';
+import { map } from 'rxjs/operators';
 
 import { ModalController } from '@ionic/angular';
-import { IParser } from '@orcha/common';
-
-type ParsedHandle = IParser<Handle, typeof ViewProfileInfoQuery>;
-
-interface State {
-  profiles: ParsedHandle[];
-}
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-modal-comments',
   templateUrl: 'modal-comments.component.html',
 })
-export class ModalCommentComponent extends StatefulComponent<State> {
-  @Input() 
-  post!: PostStoreModel;
+export class ModalCommentComponent implements OnInit {
+  @Input() post!: PostStoreModel;
   msg!: string;
   user!: UserFacade;
-  constructor(
-    private modalCtrl: ModalController,
-  ) { super({ profiles: [] }); }
+  profilePic$!: Observable<string | null>;
+  name$!: Observable<string>;
+constructor(
+  private modalCtrl: ModalController,
+  private userFacade: UserFacade
+) {
+  this.user = userFacade;
+}
+
+ngOnInit() {
+  this.profilePic$ = this.user.session.selectors.changeMaker$.pipe(
+    map(changeMaker => changeMaker?.profilePicFilePath || null)
+  );
+
+  this.name$ = this.user.session.selectors.changeMaker$.pipe(
+    map(changeMaker => `${changeMaker?.firstName || ''} ${changeMaker?.lastName || ''}` || '')
+  );
+
+  // In Progress - Need to figure out how to show new comments as they are created
+  
+  // // Subscribe to the comments$ observable to get the initial comments list
+  // this.user.comments.selectors.comments$.subscribe(comments => {
+  //   this.post.comments = comments;
+  // });
+
+  // // Subscribe to the comments$ observable to get new comments as they are created
+  // this.user.comments.selectors.comments$.subscribe(newComment => {
+  //   // Push the new comment to the comments list of the post
+  //   this.post.comments.push(newComment);
+  // });
+}
 
   comment() {
-    console.log('creating comment');
     this.user.comments.dispatchers.createComment({
         postId: this.post.id,
         text: this.msg,
         commentsId: '',
     });
-    console.log('comment created');
     this.msg = '';
+  }
+
+  getProfilePic(): Observable<string> {
+    return this.user.session.selectors.changeMaker$.pipe(
+      map(changeMaker => changeMaker?.profilePicFilePath || '')
+    );
+  }
+  
+  getName(): Observable<string> {
+    return this.user.session.selectors.changeMaker$.pipe(
+      map(changeMaker => `${changeMaker?.firstName || ''} ${changeMaker?.lastName || ''}` || '')
+    );
   }
 
   cancel() {
     return this.modalCtrl.dismiss(null, 'cancel');
-  }
-
-  // inspired by chat component
-  getProfilePic(handleId: string) {
-    const handle = this.state.profiles.find((p) => p.id === handleId);
-    return (
-      handle?.changeMaker?.profilePicFilePath ||
-      handle?.servePartner?.logoFilePath ||
-      handle?.exchangePartner?.logoFilePath ||
-      ''
-    );
-  }
-
-  // inspired by chat component
-  getName(handleId: string) {
-    const handle = this.state.profiles.find((p) => p.id === handleId);
-    return (
-      `${handle?.changeMaker?.firstName || ''} ${handle?.changeMaker?.lastName || ''}` ||
-      handle?.servePartner?.name ||
-      handle?.exchangePartner?.name ||
-      ''
-    );
   }
 
 }
