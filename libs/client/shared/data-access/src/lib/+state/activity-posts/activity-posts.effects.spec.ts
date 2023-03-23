9596bc2906169da181f53ddc15ab0b55666be2ac
Tyler Lowe <tylerlowe@gmail.com>
2023-03-22 22:27:35 -0400

    Add NgRx ActivityPost Effect Tests

--- libs/client/shared/data-access/src/lib/+state/activity-posts/activity-posts.effects.spec.ts
+++ libs/client/shared/data-access/src/lib/+state/activity-posts/activity-posts.effects.spec.ts
@@ -1,5 +1,5 @@
 import { StatusService } from "@involvemint/client/shared/util";
-import { Observable, of } from "rxjs";
+import { asyncScheduler, Observable, of, throwError } from "rxjs";
 import { ActivityPostOrchestration } from "../../orchestrations";
 import { PostEffects } from "./activity-posts.effects";
 import { Action } from "@ngrx/store";
@@ -8,6 +8,7 @@ import { TestBed } from "@angular/core/testing";
 import { hot, cold } from 'jest-marbles';
 
 import * as PostsActions from './activity-posts.actions';
+
 
 describe('Activity-Post Effects', () => {
 
@@ -23,12 +24,19 @@ describe('Activity-Post Effects', () => {
                 provideMockActions(() => actions),
                 {
                     provide: StatusService,
-                    useValue: {}
+                    useValue: {
+                        showLoader: jest.fn(),
+                        dismissLoader: jest.fn(),
+                        presentNgRxActionAlert: jest.fn(),
+                    }
                 },
                 {
                     provide: ActivityPostOrchestration,
                     useValue: {
                         list: jest.fn(),
+                        create: jest.fn(),
+                        like: jest.fn(),
+                        unlike: jest.fn(),
                     }
                 }
             ]
@@ -38,6 +46,16 @@ describe('Activity-Post Effects', () => {
 
         status = moduleRef.get(StatusService);
         posts = moduleRef.get(ActivityPostOrchestration);
+
+        // mock generic functions for all tests
+        jest.spyOn(status, 'presentNgRxActionAlert')
+            .mockImplementation(async (_action: any, error: any) => {
+                return;
+            });
+        jest.spyOn(status, 'showLoader')
+            .mockImplementation(async () => { return });
+        jest.spyOn(status, 'dismissLoader')
+            .mockImplementation(async () => { return });
     });
 
     it('should be defined', () => {
@@ -73,12 +91,135 @@ describe('Activity-Post Effects', () => {
 
     it('should return load posts error on unhappy path', () => {
         // provide fake data that will be used in effect
+        const action = PostsActions.loadPosts({ page: 1 });
+        const completion = PostsActions.loadPostsError({ error: undefined as any });
 
         // mock `this.posts.list` call and return OBSERVABLE data!
+        jest.spyOn(posts, 'list')
+            .mockImplementation((_query: any) => {
+                throw throwError("error");
+            });
+
+        // mock the observable action passed to effects
+        actions = hot('--a-', { a: action });
 
         // specify expected output and run test
-
-        // specify expected output and run test
+        const expected = cold('--(b)', { b: completion });
+        expect(effects.loadPosts$).toBeObservable(expected);
     });
+
+    it('should return like post success on happy path', () => {
+        // provide fake data that will be used in effect
+        const fakePost = { id: "1" } as any
+        const action = PostsActions.like({ dto: { postId: "1" }});
+        const completion = PostsActions.likeSuccess({ post: fakePost })
+
+        // mock `this.posts.list` call and return OBSERVABLE data!
+        jest.spyOn(posts, 'like')
+            .mockImplementation((_query: any, _dto: any) => {
+                return of(fakePost);
+            });
+
+        // specify expected output and run test
+        actions = hot('--a-', { a: action });
+        
+        // specify expected output and run test
+        const expected = cold('--(b)', { b: completion });
+        expect(effects.likePost$).toBeObservable(expected);
+    });
+
+    it('should return like post error on unhappy path', () => {
+        const action = PostsActions.like({ dto: { postId: "1" }});
+        const completion = PostsActions.likeError({ error: undefined as any });
+
+        jest.spyOn(posts, 'like')
+            .mockImplementation((_query: any, _dto: any) => {
+                throw throwError("error");
+            });
+
+        // specify expected output and run test
+        actions = hot('--a-', { a: action });
+        
+        // specify expected output and run test
+        const expected = cold('--(b)', { b: completion });
+        expect(effects.likePost$).toBeObservable(expected);
+    });
+
+    it('should return unlike post success on happy path', () => {
+        // provide fake data that will be used in effect
+        const fakePost = { id: "1" } as any
+        const action = PostsActions.unlike({ dto: { postId: "1" }});
+        const completion = PostsActions.unlikeSuccess({ post: fakePost })
+
+        // mock `this.posts.list` call and return OBSERVABLE data!
+        jest.spyOn(posts, 'unlike')
+            .mockImplementation((_query: any, _dto: any) => {
+                return of(fakePost);
+            });
+
+        // specify expected output and run test
+        actions = hot('--a-', { a: action });
+        
+        // specify expected output and run test
+        const expected = cold('--(b)', { b: completion });
+        expect(effects.unlikePost$).toBeObservable(expected);
+    });
+
+    it('should return unlike post error on unhappy path', () => {
+        const action = PostsActions.unlike({ dto: {postId: "1" }});
+        const completion = PostsActions.unlikeError({ error: undefined as any });
+
+        jest.spyOn(posts, 'unlike')
+            .mockImplementation((_query: any, _dto: any) => {
+                throw throwError("error");
+            });
+
+        // specify expected output and run test
+        actions = hot('--a-', { a: action });
+        
+        // specify expected output and run test
+        const expected = cold('--(b)', { b: completion });
+        expect(effects.unlikePost$).toBeObservable(expected);
+    });
+
+    // it('should return create post success on happy path', () => {
+    //     // provide fake data that will be used in effect
+    //     const fakePost = { id: "1"} as any;
+    //     const action = PostsActions.createPost({ dto: { poiId: "21"}})
+    //     const completion = PostsActions.createPostSuccess({
+    //         post: fakePost
+    //     });
+
+    //     // mock `this.posts.list` call and return OBSERVABLE data!
+    //     jest.spyOn(posts, 'create')
+    //         .mockImplementation((_query: any, _dto: any) => {
+    //             return of(fakePost);
+    //         });
+
+    //     actions = hot('--a-', { a: action });
+
+    //     // specify expected output and run test
+    //     const expected = cold('--(b)', { b: completion });
+    //     const actual = effects.createPost$;
+    //     expect(actual).toBeObservable(expected);
+    // });
+
+    // it('should return create post error on unhappy path', () => {
+    //     const action = PostsActions.createPost({ dto: { poiId: "21"}})
+    //     const completion = PostsActions.createPostError({ error: undefined as any });
+
+    //     // mock `this.posts.list` call and return OBSERVABLE data!
+    //     jest.spyOn(posts, 'create')
+    //         .mockImplementation((_query: any, _dto: any) => {
+    //             throw throwError("error");
+    //         });
+
+    //     actions = hot('--a-', { a: action });
+
+    //     // specify expected output and run test
+    //     const expected = cold('--(b)', { b: completion });
+    //     const actual = effects.createPost$;
+    //     expect(actual).toBeObservable(expected);
+    // });
 
 });
