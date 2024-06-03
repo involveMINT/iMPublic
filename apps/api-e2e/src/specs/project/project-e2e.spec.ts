4e1553b84a48dc3195f6b4317fcf2a70bc9faca0
QuinnNTonic <33108110+QuinnNTonic@users.noreply.github.com>
2024-06-03 00:51:59 -0400

    Win24 involvemint (#307) (#308)
    Co-authored-by: Johnson Luong <jkl53@sfu.ca>
    Co-authored-by: Johnson Luong <38903775+JohnsonL111@users.noreply.github.com>

--- apps/api-e2e/src/specs/project/project-e2e.spec.ts
+++ apps/api-e2e/src/specs/project/project-e2e.spec.ts
@@ -3,34 +3,27 @@ import {
   ServeAdminRepository,
   ServePartnerRepository,
 } from '@involvemint/server/core/domain-services';
-import { IProjectOrchestration, IUserOrchestration, Project, ServePartner } from '@involvemint/shared/domain';
+import { DTO_KEY, IParser, Project, QUERY_KEY, ServePartner, TOKEN_KEY, createQuery } from '@involvemint/shared/domain';
 import { HttpStatus } from '@nestjs/common';
 import { NestFastifyApplication } from '@nestjs/platform-fastify';
 import { Test } from '@nestjs/testing';
-import { createQuery, IParser } from '@orcha/common';
-import { ITestOrchestration } from '@orcha/testing';
 import * as uuid from 'uuid';
 import { AppTestModule } from '../../core/app-test.module';
 import { DatabaseService } from '../../core/database.service';
 import { createServeAdmin } from '../serve-admin/serve-admin.helpers';
 import { createServePartner } from '../serve-partner/serve-partner.helpers';
-import { createUserOrchestration } from '../user/user.orchestration';
-import { createProjectOrchestration } from './project.orchestration';
 import supertest from 'supertest'
 
-describe('ChangeMaker Orchestration Integration Tests', () => {
+describe('Project Integration Tests', () => {
   let app: NestFastifyApplication;
   let db: DatabaseService;
-
-  let userOrcha: ITestOrchestration<IUserOrchestration>;
-  let projectOrcha: ITestOrchestration<IProjectOrchestration>;
 
   let spRepo: ServePartnerRepository;
   let saRepo: ServeAdminRepository;
   let projectRepo: ProjectRepository;
 
   const creds = { id: 'email@email.com', password: 'GoodPwd@341' };
-  let auth: { body: { token: string }; statusCode: HttpStatus };
+  let token: string;
 
   const spQuery = createQuery<ServePartner>()({ id: true });
   let sp: IParser<ServePartner, typeof spQuery>;
@@ -47,9 +40,6 @@ describe('ChangeMaker Orchestration Integration Tests', () => {
     app = moduleRef.createNestApplication();
     db = moduleRef.get(DatabaseService);
 
-    userOrcha = createUserOrchestration(app);
-    projectOrcha = createProjectOrchestration(app);
-
     spRepo = moduleRef.get(ServePartnerRepository);
     saRepo = moduleRef.get(ServeAdminRepository);
     projectRepo = moduleRef.get(ProjectRepository);
@@ -59,11 +49,28 @@ describe('ChangeMaker Orchestration Integration Tests', () => {
 
   beforeEach(async () => {
     await db.clearDb();
-    auth = await userOrcha.signUp({ token: true }, '', creds);
+
+    const signUpResult = await supertest(app.getHttpServer())
+      .post('/user/signUp')
+      .send({
+        [QUERY_KEY]: { [TOKEN_KEY]: true },
+        [DTO_KEY]: creds,
+      });
+    token = signUpResult.body[TOKEN_KEY];
     sp = await createServePartner(spQuery, spRepo, { id: uuid.v4(), handle: 'spHandle' });
     await createServeAdmin({}, saRepo, creds.id, sp.id);
-    const { body } = await projectOrcha.create(projectQuery, auth.body.token, { spId: sp.id });
-    project = body;
+
+    const projectCreationResult = await supertest(app.getHttpServer())
+    .post('/project/create')
+    .set('token', token)
+    .set('Content-Type', 'application/json')
+    .set('Accept', 'application/json')
+    .send({ 
+      [QUERY_KEY]: projectQuery,
+      [DTO_KEY]: { spId: sp.id }
+    });
+
+    project = projectCreationResult.body;
   });
 
   afterAll(async () => await app.close());
@@ -71,45 +78,103 @@ describe('ChangeMaker Orchestration Integration Tests', () => {
   describe('getAll', () => {
     it('should not get private projects (project is private by default)', async () => {
 
-      const result = await supertest(app.getHttpServer())
-      .post(`/orcha/project/getAll`)
-      .field('query', JSON.stringify(projectQuery));
-
-      expect(result.body.length).toBe(0);
+      const { body } = await supertest(app.getHttpServer())
+      .post('/project/getAll')
+      .set('token', token)
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+      .send({ 
+        [QUERY_KEY]: projectQuery,
+        [DTO_KEY]: {}
+      });
+      
+      expect(body.length).toBe(0);
     });
     it('should not get unlisted projects', async () => {
       await projectRepo.update(project.id, { listingStatus: 'unlisted' });
-      const res = await projectOrcha.getAll(projectQuery, auth.body.token, {});
-      expect(res.body.length).toBe(0);
+      
+      const { body } = await supertest(app.getHttpServer())
+      .post('/project/getAll')
+      .set('token', token)
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+      .send({ 
+        [QUERY_KEY]: projectQuery,
+        [DTO_KEY]: {}
+      });
+      
+      expect(body.length).toBe(0);
     });
     it('should get public projects', async () => {
       await projectRepo.update(project.id, { listingStatus: 'public' });
-      const res = await projectOrcha.getAll(projectQuery, auth.body.token, {});
-      expect(res.body[0].id).toBe(project.id);
+      
+      const { body } = await supertest(app.getHttpServer())
+      .post('/project/getAll')
+      .set('token', token)
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+      .send({ 
+        [QUERY_KEY]: projectQuery,
+        [DTO_KEY]: {}
+      });
+      
+      expect(body[0].id).toBe(project.id);
     });
   });
 
   describe('getOne', () => {
     it('should not get private project (project is private by default)', async () => {
-      const res = await projectOrcha.getOne(projectQuery, auth.body.token, { projectId: project.id });
-      expect(res.statusCode).toBe(HttpStatus.UNAUTHORIZED);
+      const result = await supertest(app.getHttpServer())
+      .post('/project/getOne')
+      .set('token', token)
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+      .send({ 
+        [QUERY_KEY]: projectQuery,
+        [DTO_KEY]: { projectId: project.id }
+      });
+      expect(result.statusCode).toBe(HttpStatus.UNAUTHORIZED);
     });
     it('should get unlisted project', async () => {
       await projectRepo.update(project.id, { listingStatus: 'unlisted' });
-      const res = await projectOrcha.getOne(projectQuery, auth.body.token, { projectId: project.id });
-      expect(res.body.id).toBe(project.id);
+      const result = await supertest(app.getHttpServer())
+      .post('/project/getOne')
+      .set('token', token)
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+      .send({ 
+        [QUERY_KEY]: projectQuery,
+        [DTO_KEY]: { projectId: project.id }
+      });
+      expect(result.body.id).toBe(project.id);
     });
     it('should get public project', async () => {
       await projectRepo.update(project.id, { listingStatus: 'public' });
-      const res = await projectOrcha.getOne(projectQuery, auth.body.token, { projectId: project.id });
-      expect(res.body.id).toBe(project.id);
+      const result = await supertest(app.getHttpServer())
+      .post('/project/getOne')
+      .set('token', token)
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+      .send({ 
+        [QUERY_KEY]: projectQuery,
+        [DTO_KEY]: { projectId: project.id }
+      });
+      expect(result.body.id).toBe(project.id);
     });
   });
 
   describe('getAllOwnedBySp', () => {
     it('should get only projects owned by me', async () => {
-      const res = await projectOrcha.getAllOwnedBySp(projectQuery, auth.body.token, { spId: sp.id });
-      expect(res.body[0].id).toBe(project.id);
+      const result = await supertest(app.getHttpServer())
+      .post('/project/getAllOwnedBySp')
+      .set('token', token)
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+      .send({ 
+        [QUERY_KEY]: projectQuery,
+        [DTO_KEY]: { spId: sp.id }
+      });
+      expect(result.body[0].id).toBe(project.id);
     });
     it('should not get projects not owned by me', async () => {
       const newSpId = uuid.v4();
@@ -142,8 +207,16 @@ describe('ChangeMaker Orchestration Integration Tests', () => {
         spQuery
       );
       await projectRepo.update(project.id, { servePartner: newSpId });
-      const res = await projectOrcha.getAllOwnedBySp(projectQuery, auth.body.token, { spId: sp.id });
-      expect(res.body.length).toBe(0);
+      const result = await supertest(app.getHttpServer())
+      .post('/project/getAllOwnedBySp')
+      .set('token', token)
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+      .send({ 
+        [QUERY_KEY]: projectQuery,
+        [DTO_KEY]: { spId: sp.id }
+      });
+      expect(result.body.length).toBe(0);
     });
   });
 
@@ -156,10 +229,18 @@ describe('ChangeMaker Orchestration Integration Tests', () => {
   describe('delete', () => {
     it('should delete', async () => {
       expect(await projectRepo.findOne(project.id)).toBeTruthy();
-      expect(
-        (await projectOrcha.delete({ deletedId: true }, auth.body.token, { projectId: project.id })).body
-          .deletedId
-      ).toBe(project.id);
+
+      const result = await supertest(app.getHttpServer())
+      .post('/project/delete')
+      .set('token', token)
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+      .send({ 
+        [QUERY_KEY]: { deletedId: true },
+        [DTO_KEY]: { projectId: project.id }
+      });
+
+      expect(result.body.deletedId).toBe(project.id);
       expect(await projectRepo.findOne(project.id)).toBeFalsy();
     });
   });
