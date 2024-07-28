e727a2d4bfe7a19a8731c4887425f84d6664d4cc
BKtheInfamous <134336066+BKtheInfamous@users.noreply.github.com>
2024-07-28 10:51:31 -0700

    add export of geolocationService to index

--- libs/client/shared/util/src/index.ts
+++ libs/client/shared/util/src/index.ts
@@ -1,6 +1,7 @@
 export * from './lib/confirm-deactivation.guard';
 export * from './lib/file.helpers';
 export * from './lib/geolocation';
+export * from './lib/geolocation.service';
 export * from './lib/info-modal/info-modal.module';
 export * from './lib/modals.animations';
 export * from './lib/popover/popover-ref';
