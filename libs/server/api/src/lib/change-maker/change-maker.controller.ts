e258f798849339ee61292672826947c4dbb13856
jackson-dfg <jacksonchen92@developforgood.org>
2025-01-26 12:38:16 -0500

    Win25/issue 376 (#377)
    This relates to HLI 3, issue
    https://github.com/involveMINT/iMPublic/issues/376
    
    Changes include:
    
    1. Updating the `changeMakerService` class in change-maker.service.ts
    for the backend
    
    I updated the `createProfile` function to retrieve data from
    `ExchangePartnerRepository` (for business admins) and
    `ServePartnerRepository` (for project admins)
    
    I added a `getPrePopulatedData` function, which retrieves data for the
    frontend.
    
    The change maker controller in change-maker.controller.ts was also
    updated accordingly.
    
    2. Updating change-maker.controller.ts for the frontend
    
    By calling the previously defined `getPrePopulatedData`, it populates
    the text fields if there is data.
    
    ---------
    
    Co-authored-by: Zanzao Chen <zanzaoc@andrew.cmu.edu>

--- libs/server/api/src/lib/change-maker/change-maker.controller.ts
+++ libs/server/api/src/lib/change-maker/change-maker.controller.ts
@@ -12,33 +12,34 @@ import {
   UserQuery,
 } from '@involvemint/shared/domain';
 import {
-    Controller,
-    Post,
-    Body,
-    UseInterceptors,
-    UploadedFile,
-    Headers
-  } from '@nestjs/common';
+  Controller,
+  Post,
+  Get,
+  Body,
+  Headers,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { QueryValidationPipe, ValidationPipe } from '../pipes';
 
 @Controller(InvolvemintRoutes.changeMaker)
 export class ChangeMakerController {
-    constructor(private readonly cmService: ChangeMakerService) {}
+  constructor(private readonly cmService: ChangeMakerService) {}
 
   @Post('createProfile')
   createProfile(
-    @Body(QUERY_KEY, new QueryValidationPipe(UserQuery.changeMaker)) query: IQuery<ChangeMaker>, 
-    @Headers(TOKEN_KEY) token: string, 
+    @Body(QUERY_KEY, new QueryValidationPipe(UserQuery.changeMaker)) query: IQuery<ChangeMaker>,
+    @Headers(TOKEN_KEY) token: string,
     @Body(DTO_KEY, new ValidationPipe()) dto: CreateChangeMakerProfileDto
   ) {
-      return this.cmService.createProfile(query, token, dto);
+    return this.cmService.createProfile(query, token, dto);
   }
 
   @Post('editProfile')
   async editProfile(
-    @Body(QUERY_KEY, new QueryValidationPipe(UserQuery.changeMaker)) query: IQuery<ChangeMaker>, 
-    @Headers(TOKEN_KEY) token: string, 
+    @Body(QUERY_KEY, new QueryValidationPipe(UserQuery.changeMaker)) query: IQuery<ChangeMaker>,
+    @Headers(TOKEN_KEY) token: string,
     @Body(DTO_KEY, new ValidationPipe()) dto: EditCmProfileDto
   ) {
     return this.cmService.editProfile(query, token, dto);
@@ -47,10 +48,15 @@ export class ChangeMakerController {
   @Post('updateProfileImage')
   @UseInterceptors(FileInterceptor(FILES_KEY))
   async updateProfileImage(
-    @Body(QUERY_KEY, new QueryValidationPipe(UserQuery.changeMaker)) query: IQuery<ChangeMaker>, 
-    @Headers(TOKEN_KEY) token: string, 
+    @Body(QUERY_KEY, new QueryValidationPipe(UserQuery.changeMaker)) query: IQuery<ChangeMaker>,
+    @Headers(TOKEN_KEY) token: string,
     @UploadedFile() file: Express.Multer.File
   ) {
     return this.cmService.updateProfileImage(query, token, file);
+  }
+
+  @Get('prepopulate')
+  async getPrePopulatedData(@Headers(TOKEN_KEY) token: string) {
+    return this.cmService.getPrePopulatedData(token);
   }
 }
