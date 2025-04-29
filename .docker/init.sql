c64801587723d484bc73f04e2d1f279cffdb9b87
Quinn <33108110+QuinnNTonic@users.noreply.github.com>
2025-04-28 20:38:29 -0400

    feat: I have made minor changes to work with the automated tests.

--- .docker/init.sql
+++ .docker/init.sql
@@ -1,6 +1,5 @@
-SELECT 'CREATE DATABASE involvemint'
+SELECT 'CREATE DATABASE "involvemint"'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'involvemint')\gexec
 
-SELECT 'CREATE DATABASE involvemint-e2e'
+SELECT 'CREATE DATABASE "involvemint-e2e"'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'involvemint-e2e')\gexec
-
