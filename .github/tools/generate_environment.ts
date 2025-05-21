11e52807490d7c1d3b04c3c809b5685382b1e1e1
Quinn <33108110+QuinnNTonic@users.noreply.github.com>
2025-05-21 18:24:44 -0400

    Working through deploy issue

--- .github/tools/generate_environment.ts
+++ .github/tools/generate_environment.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import { Env } from '../libs/shared/domain/src/lib/environments/environment.interface';
+import { Env } from '../../libs/shared/domain/src/lib/environments/environment.interface';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { FirebaseScryptOptions } from 'firebase-scrypt';
 import * as geocoder from 'node-geocoder';
