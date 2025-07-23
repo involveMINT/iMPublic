5cf505e31e7689b176837140e97539ab43555381
Quinn <33108110+QuinnNTonic@users.noreply.github.com>
2025-07-23 13:23:31 -0400

    feat: Fixing issue with the package-lock.json not being up to date.

--- util/ormconfig.js
+++ util/ormconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   type: 'postgres',
-  host: process.env.DB_HOST,
+  host: '127.0.0.1', //process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT || '5432'),
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
